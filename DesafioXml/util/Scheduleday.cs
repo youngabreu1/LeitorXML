faa8a1f1bfefd0483a8b791867151cfd5fd603b8
Gabriel Carlos <gabrielcarlos0705@gmail.com>
2023-08-22 17:31:08 -0300

    Implementação dos atributos das classes Inserção e Break, e a função Print

--- DesafioXml/util/Scheduleday.cs
+++ DesafioXml/util/Scheduleday.cs
@@ -1,58 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
+
 
 namespace DesafioXml.util
 {
-    
+
     public class ScheduleDay
     {
         public string Pathfile { get; set; }
-        private XmlDocument document;
-        
+        public XmlDocument document;
+
         public ScheduleDay(string path)
         {
-            
-            Pathfile = path;
-            readXML();
-            
-        }
-        public void readXML()
-        {
-            document = new XmlDocument();
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            document.Load(Pathfile);
-        }
-        public XmlNodeList printBreak()
-        {
-            
-            XmlNodeList breakNodes = document.SelectNodes("//*[starts-with(local-name(), 'Break')]");
-            Console.WriteLine("Break:");
 
-            foreach (XmlNode breakNode in breakNodes)
+            Pathfile = path;
+            ReadXML();
+        }
+
+        public void ReadXML()
+        {
+
+            XmlDocument document = new XmlDocument();
+            document.Load(Pathfile);
+
+            //void printInsercoes()
+            //{
+
+            //    XmlNodeList insercaoElements = document.SelectNodes("//*[starts-with(local-name(), 'Ins')]");
+            //    foreach (XmlNode insercaoElement in insercaoElements)
+            //    {
+            //        Insercao insInstance = new Insercao(insercaoElement);
+            //        Console.WriteLine(insInstance);
+            //    }
+
+            //}
+
+            XmlNodeList breakElements = document.SelectNodes("//*[starts-with(local-name(), 'Break')]");
+            foreach (XmlNode breakElement in breakElements)
             {
-                //var tt = breakNode.ChildNodes;
-                Console.WriteLine("\t" + breakNode.OuterXml);
+
+                Break breakInstance = new Break(breakElement);
+                Console.WriteLine(breakInstance);
 
             }
-            return breakNodes;
- 
-            
+
         }
-        public void breakTeste()
-        {
-            Break breaks = new Break(printBreak());
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //public XmlNodeList printBreak()
+        //{
+
+        //    XmlNodeList breakNodes = document.SelectNodes("//*[starts-with(local-name(), 'Break')]");
+        //    Console.WriteLine("Break:");
+
+        //    foreach (XmlNode breakNode in breakNodes)
+        //    {
+        //        //var tt = breakNode.ChildNodes;
+        //        Console.WriteLine("\t" + breakNode.OuterXml);
+
+        //    }
+        //    return breakNodes;
+
+
+        //}
+        //public void breakTeste()
+        //{
+        //    Break breaks = new Break(printBreak());
+        //}
 
 
     }
 }
-
-//public override string ToString()
-//{
-//    return "penis";
-//}
