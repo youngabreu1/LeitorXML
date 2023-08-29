18bf27638a99e6a1fbeea0aed7542b587bdf57a5
Gabriel Carlos <gabrielcarlos0705@gmail.com>
2023-08-29 17:48:35 -0300

    Retirei comentários e alinhei os códigos

--- DesafioXml/util/Scheduleday.cs
+++ DesafioXml/util/Scheduleday.cs
@@ -15,11 +15,13 @@ namespace DesafioXml.util
     public class ScheduleDay
     {
         public string Pathfile { get; set; }
+
         public List<Break> Breaks { get; set; }
         public XmlDocument document;
 
         public ScheduleDay(string path)
         {
+
             Breaks = new List<Break>();
             ReadXML(path);
 
@@ -29,40 +31,17 @@ namespace DesafioXml.util
         {
             XmlDocument document = new XmlDocument();
             document.Load(path);
-            //XmlNodeList insercaoElements = document.SelectNodes("//*[starts-with(local-name(), 'Ins')]");
-            
+
             XmlNodeList breakElements = document.SelectNodes("//*[starts-with(local-name(), 'Break')]");
             foreach (XmlNode breakElement in breakElements)
             {
                 Break breakInstance = new Break(breakElement);
                 Breaks.Add(breakInstance);
                 Console.WriteLine(breakInstance);
-                XmlNode copyBreakElement = breakElement.CloneNode(true); // Cria uma cópia do nó
+                XmlNode copyBreakElement = breakElement.CloneNode(true);
                 breakInstance.ListaInsercao(copyBreakElement);
             }
         }
-
-        //public XmlNodeList printBreak()
-        //{
-
-        //    XmlNodeList breakNodes = document.SelectNodes("//*[starts-with(local-name(), 'Break')]");
-        //    Console.WriteLine("Break:");
-
-        //    foreach (XmlNode breakNode in breakNodes)
-        //    {
-        //        //var tt = breakNode.ChildNodes;
-        //        Console.WriteLine("\t" + breakNode.OuterXml);
-
-        //    }
-        //    return breakNodes;
-
-
-        //}
-        //public void breakTeste()
-        //{
-        //    Break breaks = new Break(printBreak());
-        //}
-
 
     }
 }
